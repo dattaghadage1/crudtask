d528cfdd80879c02b0f6cac11e58db162aea2e20
Datta Ghadage <dattaghadage9696.com>
2023-03-27 19:30:05 +0530

    second

--- Crud/Updateemp.jsx
+++ Crud/Updateemp.jsx
@@ -3,49 +3,49 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { UPDATEEMP } from './Redux/Reducer/Reducer1'
 function Updateemp() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const { pathname } = useLocation()
-  const empid = pathname.replace("/updateemp/", "")
-  const employee = useSelector((state) => state.users.users.find((emp) => emp.id === empid))
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+    const empid = pathname.replace("/updateemp/", "")
+    const employee = useSelector((state) => state.users.users.find((emp) => emp.id === empid))
 
-  const [data, setdata] = useState({
-    id: empid,
-    name: employee.name,
-    age: employee.age,
-    city: employee.city
-  })
+    const [data, setdata] = useState({
+        id: empid,
+        name: employee.name,
+        age: employee.age,
+        city: employee.city
+    })
 
-  const getInput = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setdata({ ...data, [name]: value })
+    const getInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setdata({ ...data, [name]: value })
 
-  }
-  const SendData = (e) => {
-    e.preventDefault()
-    dispatch(UPDATEEMP(data))
-    navigate('/')
-  }
-  return (
-    <div>
-      <form onSubmit={SendData}>
-        <div className="row" style={{ display: "flex", justifyContent: "center" }}>
-          <div className="four columns" style={{ textAlign: "center" }}>
-           <label htmlFor="exampletextInput">Id</label> 
-           <input className="u-full-width" type="text" placeholder="id" value={empid} name='id'  onChange={(e)=>getInput(e)} /> 
-            <label htmlFor="exampletextInput">Name</label>
-            <input className="u-full-width" type="text" placeholder="name" value={data.name} name="name" onChange={(e) => getInput(e)} />
-            <label htmlFor="exampletextInput">Age</label>
-            <input className="u-full-width" type="text" placeholder="age" name='age' value={data.age} onChange={(e) => getInput(e)} />
-            <label htmlFor="exampletextInput">City</label>
-            <input className="u-full-width" type="text" placeholder="city" name='city' value={data.city} onChange={(e) => getInput(e)} />
-            <input className="button-primary" type="submit" defaultValue="Submit" />
-          </div>
+    }
+    const SendData = (e) => {
+        e.preventDefault()
+        dispatch(UPDATEEMP(data))
+        navigate('/')
+    }
+    return (
+        <div>
+            <form onSubmit={SendData}>
+                <div className="row" style={{ display: "flex", justifyContent: "center" }}>
+                    <div className="four columns" style={{ textAlign: "center" }}>
+                        <label htmlFor="exampletextInput">Id</label>
+                        <input className="u-full-width" type="text" placeholder="id" value={empid} name='id' onChange={getInput} />
+                        <label htmlFor="exampletextInput">Name</label>
+                        <input className="u-full-width" type="text" placeholder="name" value={data.name} name="name" onChange={getInput} />
+                        <label htmlFor="exampletextInput">Age</label>
+                        <input className="u-full-width" type="text" placeholder="age" name='age' value={data.age} onChange={getInput} />
+                        <label htmlFor="exampletextInput">City</label>
+                        <input className="u-full-width" type="text" placeholder="city" name='city' value={data.city} onChange={getInput} />
+                        <input className="button-primary" type="submit" defaultValue="Submit" />
+                    </div>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default Updateemp
--- Crud/adduser.jsx
+++ Crud/adduser.jsx
@@ -3,53 +3,53 @@ import { useDispatch } from 'react-redux';
 import { ADDUSER } from './Redux/Reducer/Reducer1';
 import { useNavigate } from 'react-router-dom';
 function Adduser() {
-  const Navigate=useNavigate()
-  const dispatch=useDispatch();
-  const[data,setdata]=useState({
-    id:"",
-    name:"",
-    age:"",
-    city:""
+  const Navigate = useNavigate()
+  const dispatch = useDispatch();
+  const [data, setdata] = useState({
+    id: "",
+    name: "",
+    age: "",
+    city: ""
   })
 
-  const getInput=(e)=>{
-    const name=e.target.name;
-    const value=e.target.value;
-    setdata({...data,[name]:value})
+  const getInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setdata({ ...data, [name]: value })
 
   }
-  const SendData=(e)=>{
+  const SendData = (e) => {
     e.preventDefault();
     console.log(data)
-      
-      if(data.name && data.id !==""){
-        dispatch(ADDUSER(data))
-        Navigate('/')
-      }else{
-        alert("data not inserted")
-      }
-      
-      
+
+    if (data.name && data.id !== "") {
+      dispatch(ADDUSER(data))
+      Navigate('/')
+    } else {
+      alert("data not inserted")
+    }
+
+
   }
   return (
 
- <div>
-  <form onSubmit={SendData}>
-    <div className="row" style={{display:"flex",justifyContent:"center"}}>
-      <div className="four columns" style={{textAlign:"center"}}>
-      <label htmlFor="exampletextInput">Id</label>
-        <input className="u-full-width" type="text" placeholder="id" name='id'  onChange={(e)=>getInput(e)} />
-        <label htmlFor="exampletextInput">Name</label>
-        <input className="u-full-width" type="text" placeholder="name" name='name'  onChange={(e)=>getInput(e)} />
-        <label htmlFor="exampletextInput">Age</label>
-        <input className="u-full-width" type="text" placeholder="age" name='age' onChange={(e)=>getInput(e)} />
-        <label htmlFor="exampletextInput">City</label>
-        <input className="u-full-width" type="text" placeholder="city" name='city' onChange={(e)=>getInput(e)} />
-        <input className="button-primary" type="submit" defaultValue="Submit" />
-      </div>
+    <div>
+      <form onSubmit={SendData}>
+        <div className="row" style={{ display: "flex", justifyContent: "center" }}>
+          <div className="four columns" style={{ textAlign: "center" }}>
+            <label htmlFor="exampletextInput">Id</label>
+            <input className="u-full-width" type="text" placeholder="id" name='id' onChange={getInput} />
+            <label htmlFor="exampletextInput">Name</label>
+            <input className="u-full-width" type="text" placeholder="name" name='name' onChange={getInput} />
+            <label htmlFor="exampletextInput">Age</label>
+            <input className="u-full-width" type="text" placeholder="age" name='age' onChange={getInput} />
+            <label htmlFor="exampletextInput">City</label>
+            <input className="u-full-width" type="text" placeholder="city" name='city' onChange={getInput} />
+            <input className="button-primary" type="submit" defaultValue="Submit" />
+          </div>
+        </div>
+      </form>
     </div>
-  </form>
-</div>
 
   )
 }
